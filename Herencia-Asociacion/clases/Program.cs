3de210a558ccf47beca28d9da98ddebc5e125536
@Emma6891 <enriquez.eml@gmail.com>
2025-04-11 22:19:06 -0300

    Segundo comentario

--- Herencia-Asociacion/clases/Program.cs
+++ Herencia-Asociacion/clases/Program.cs
@@ -10,7 +10,7 @@ namespace clases
     {
         static void Main(string[] args)
         {
-            // comentario para registrar cambios en github
+            // comentario 2 (doS) para registrar cambios en github
             Persona p1 = new Persona();
             p1.setNom("Emmanuel");
             p1.setEdad (38);
